6e3043d8bb3e9042511ded0c3dd713b8ce67283b
Jason4931 <emmanuel.jason2008@gmail.com>
2024-07-15 12:53:20 +0700

    Prisma setup

--- be/src/index.ts
+++ be/src/index.ts
@@ -1,13 +1,37 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, Request, Response, NextFunction } from 'express';
+import { PrismaClient } from '@prisma/client';
 
 require('dotenv').config();
 
 const app: Application = express();
+const prisma = new PrismaClient()
+
+app.use(express.json()); // parse JSON requests
 
 app.get('/', async (req: Request, res: Response) => {
   res.send({ message: 'Hello World!' });
 });
 
-app.listen(process.env.PORT, () => {
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+  res.status(500).send({ message: 'Internal Server Error' });
+});
+
+async function main() {
+  try {
+    // Write your Prisma Client queries here
+    const result = await prisma.user.findMany();
+    console.log(result);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+main().catch((e) => console.error(e)).finally(() => {
+  app.listen(process.env.PORT, () => {
     console.log('Server is running on port 3000');
+  });
 });
